ef50f6d61b3153fd8e51260ea010e1424d43300e
sunliang5211 <sunliang5211@163.com>
2018-10-19 10:12:44 +0800

    修改

--- src/main/java/com/sitech/test/HelloWorld.java
+++ src/main/java/com/sitech/test/HelloWorld.java
@@ -6,5 +6,6 @@ package com.sitech.test;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello sunliang");
+        System.out.println("Hello yanyitong");
     }
 }
