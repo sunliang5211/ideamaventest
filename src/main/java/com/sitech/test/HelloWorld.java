0049559733009a234a325d3e66a7d793f2d08433
sunliang5211 <sunliang5211@163.com>
2018-10-19 09:45:23 +0800

    Initial commit

---
+++ src/main/java/com/sitech/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sitech.test;
+
+/**
+ * Created by Thinkpad on 2018/10/19.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello sunliang");
+    }
+}
